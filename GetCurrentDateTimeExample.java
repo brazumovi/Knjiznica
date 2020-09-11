1c08944adf14bc1fc51bdbf980fa54e775f26c90
brazumovi <71143836+brazumovi@users.noreply.github.com>
2020-09-11 19:51:35 +0200

    Add files via upload

---
+++ GetCurrentDateTimeExample.java
@@ -0,0 +1,38 @@
+package tvz;
+
+import java.util.Calendar;
+
+/**
+ * A class used for getting current date.
+ * 
+ * @author Bruno
+ * @version 1.0, 11 Sep 2020
+ *
+ */
+public class GetCurrentDateTimeExample {
+
+	/**
+	 * Main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Calendar now = Calendar.getInstance();
+
+		System.out.println("Current Year is : " + now.get(Calendar.YEAR));
+		System.out.println("Current Month is : " + (now.get(Calendar.MONTH) + 1));
+		System.out.println("Current Date is : " + now.get(Calendar.DATE));
+
+		System.out.println("Current Hour in 12 hour format is : " + now.get(Calendar.HOUR));
+		System.out.println("Current Hour in 24 hour format is : " + now.get(Calendar.HOUR_OF_DAY));
+		System.out.println("Current Minute is : " + now.get(Calendar.MINUTE));
+		System.out.println("Current Second is : " + now.get(Calendar.SECOND));
+		System.out.println("Current Millisecond is : " + now.get(Calendar.MILLISECOND));
+
+		System.out.println("Current full date time is : " + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE)
+				+ "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE)
+				+ ":" + now.get(Calendar.SECOND) + "." + now.get(Calendar.MILLISECOND));
+	}
+}
+
